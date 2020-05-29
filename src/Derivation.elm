0aba9751204309c1ff28395251a1b9aff2ac0fff
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2020-05-29 15:28:49 +0200

    Add doc comments to Derivation.elm

--- src/Derivation.elm
+++ src/Derivation.elm
@@ -1,8 +1,17 @@
 module Derivation exposing (Graph)
 
+{-| Representation of the derivation of terms as a graph.
+NOT USED YET.
+
+@docs Graph
+
+-}
+
 import Incompatibility exposing (Incompatibility)
 
 
+{-| Derivation graph.
+-}
 type Graph
     = Leaf { external : Incompatibility }
     | Node { derived : Incompatibility, cause1 : Graph, cause2 : Graph }
