bc91d4c8864751a3e32b4e9dd34a1ab51a3ce322
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2020-07-20 16:55:07 +0200

    Add end-to-end tests provided by PubGrub documentation

---
+++ tests/EndToEnd.elm
@@ -0,0 +1,85 @@
+module EndToEnd exposing (..)
+
+import Database.Stub as Stub
+import Expect
+import PubGrub
+import Test exposing (Test)
+import Version
+
+
+noConflict : Test
+noConflict =
+    Test.test "A simple case where no actual conflicts occur" <|
+        \_ ->
+            PubGrub.solve
+                { listAvailableVersions = Stub.listAvailableVersions1
+                , getDependencies = Stub.getDependencies1
+                }
+                "root"
+                Version.one
+                |> Expect.ok
+
+
+avoidingConflictDuringDecisionMaking : Test
+avoidingConflictDuringDecisionMaking =
+    Test.test "decision making examines a package version that would cause a conflict and chooses not to select it" <|
+        \_ ->
+            PubGrub.solve
+                { listAvailableVersions = Stub.listAvailableVersions2
+                , getDependencies = Stub.getDependencies2
+                }
+                "root"
+                Version.one
+                |> Expect.ok
+
+
+performingConflictResolution : Test
+performingConflictResolution =
+    Test.test "full conflict resolution" <|
+        \_ ->
+            PubGrub.solve
+                { listAvailableVersions = Stub.listAvailableVersions3
+                , getDependencies = Stub.getDependencies3
+                }
+                "root"
+                Version.one
+                |> Expect.ok
+
+
+conflictResolutionWithPartialSatisfier : Test
+conflictResolutionWithPartialSatisfier =
+    Test.test "conflict resolution where the term in question isn't totally satisfied by a single satisfier" <|
+        \_ ->
+            PubGrub.solve
+                { listAvailableVersions = Stub.listAvailableVersions4
+                , getDependencies = Stub.getDependencies4
+                }
+                "root"
+                Version.one
+                |> Expect.ok
+
+
+linearErrorReporting : Test
+linearErrorReporting =
+    Test.test "error reporting when the derivation graph is straightforwardly linear" <|
+        \_ ->
+            PubGrub.solve
+                { listAvailableVersions = Stub.listAvailableVersions5
+                , getDependencies = Stub.getDependencies5
+                }
+                "root"
+                Version.one
+                |> Expect.err
+
+
+branchingErrorReporting : Test
+branchingErrorReporting =
+    Test.test "too complex to explain in a linear chain of reasoning" <|
+        \_ ->
+            PubGrub.solve
+                { listAvailableVersions = Stub.listAvailableVersions6
+                , getDependencies = Stub.getDependencies6
+                }
+                "root"
+                Version.one
+                |> Expect.err
